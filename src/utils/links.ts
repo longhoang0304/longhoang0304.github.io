5adda2b82e55274d0b34c2e8254a754ecd2a617a
Long Hoàng <longhoang0304@users.noreply.github.com>
2023-03-13 15:02:57 +0000

    [feat]: finished responsive ui

---
+++ src/utils/links.ts
@@ -0,0 +1,27 @@
+export const LINKS = [
+  {
+    iconName:"file-description",
+    href:"https://bit.ly/3sPIEcD",
+    title:"CV",
+  },
+  {
+    iconName: "user",
+    href: "https://www.longhoang.me/about/",
+    title: "About me",
+  },
+  {
+    iconName: "brand-github",
+    href: "https://github.com/longhoang0304/",
+    title: "Github",
+  },
+  {
+    iconName: "brand-linkedin",
+    href: "https://linkedin.com/in/notlonghoang/",
+    title: "Linkedin",
+  },
+  {
+    iconName: "rss",
+    href: "https://www.longhoang.me/rss/",
+    title: "RSS",
+  },
+]
