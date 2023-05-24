34f02ce0062b5d0f0bd0097e49303752bf0dc2c6
Long Hoàng <longhoang0304@users.noreply.github.com>
2023-05-24 14:05:58 +0100

    [feat] Add wasm snack game

---
+++ astro.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from "astro/config";
+
+// https://astro.build/config
+import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://longhoang.me",
+  integrations: [sitemap()],
+  base: "/",
+  trailingSlash: "ignore",
+});
